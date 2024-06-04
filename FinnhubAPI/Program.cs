c03d4d552aec4dc5e4a0ebc4e4206ce750b3dcc4
Akancha Kumari <akxa75041@gmail.com>
2024-06-04 11:12:39 +0300

    first commit

---
+++ FinnhubAPI/Program.cs
@@ -0,0 +1,73 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+
+namespace finnhubStock
+{
+    public class finnhubAPI
+    {
+        static async Task Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Please provide a stock symbol as a command-line parameter.");
+                return;
+            }
+            else
+            {
+                string symbol = args[0];
+                string ApiKey = "cop89jhr01qikslb9sogcop89jhr01qikslb9sp0";
+                string url = $"https://finnhub.io/api/v1/search?q={symbol}";
+
+                using (var client = new HttpClient())
+                {
+                    client.DefaultRequestHeaders.Accept.Clear();
+                    client.DefaultRequestHeaders.Add("X-Finnhub-Token", ApiKey);
+
+                    HttpResponseMessage response = await client.GetAsync(url);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string responseString = await response.Content.ReadAsStringAsync();
+                        Result getResult = JsonConvert.DeserializeObject<Result>(responseString);
+
+                        if (getResult != null && getResult.count > 0)
+                        {
+                            foreach (StockLine stock in getResult.result)
+                            {
+                                Console.WriteLine($"{stock.symbol} {stock.description}");
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("No results found.");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error fetching data. Please try again later.");
+                    }
+                }
+            }
+        }
+    }
+
+    public class Result
+    {
+        public int count { get; set; }
+        public StockLine[] result { get; set; }
+    }
+
+    public class StockLine
+    {
+        public string symbol { get; set; }
+        public string description { get; set; }
+    }
+
+}
+
